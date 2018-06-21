88f45ebea96563e08fb0bdf79e7aeeb6e886a21f
juliambrosman <julia@bigelow.org>
2018-06-21 10:52:12 -0400

    adds amino acid column, fixes U problem

--- codonw_parser.py
+++ codonw_parser.py
@@ -1,7 +1,74 @@
+from __future__ import print_function
+
 from collections import defaultdict
 import pandas as pd
 import re
 import click
+
+codons = {'AAA': 'Lys',
+ 'AAC': 'Asn',
+ 'AAG': 'Lys',
+ 'AAU': 'Asn',
+ 'ACA': 'Thr',
+ 'ACC': 'Thr',
+ 'ACG': 'Thr',
+ 'ACU': 'Thr',
+ 'AGA': 'Arg',
+ 'AGC': 'Ser',
+ 'AGG': 'Arg',
+ 'AGU': 'Ser',
+ 'AUA': 'Ile',
+ 'AUC': 'Ile',
+ 'AUG': 'Met',
+ 'AUU': 'Ile',
+ 'CAA': 'Gln',
+ 'CAC': 'His',
+ 'CAG': 'Gln',
+ 'CAU': 'His',
+ 'CCA': 'Pro',
+ 'CCC': 'Pro',
+ 'CCG': 'Pro',
+ 'CCU': 'Pro',
+ 'CGA': 'Arg',
+ 'CGC': 'Arg',
+ 'CGG': 'Arg',
+ 'CGU': 'Arg',
+ 'CUA': 'Leu',
+ 'CUC': 'Leu',
+ 'CUG': 'Leu',
+ 'CUU': 'Leu',
+ 'GAA': 'Glu',
+ 'GAC': 'Asp',
+ 'GAG': 'Glu',
+ 'GAU': 'Asp',
+ 'GCA': 'Ala',
+ 'GCC': 'Ala',
+ 'GCG': 'Ala',
+ 'GCU': 'Ala',
+ 'GGA': 'Gly',
+ 'GGC': 'Gly',
+ 'GGG': 'Gly',
+ 'GGU': 'Gly',
+ 'GUA': 'Val',
+ 'GUC': 'Val',
+ 'GUG': 'Val',
+ 'GUU': 'Val',
+ 'UAA': 'TER',
+ 'UAC': 'Tyr',
+ 'UAG': 'TER',
+ 'UAU': 'Tyr',
+ 'UCA': 'Ser',
+ 'UCC': 'Ser',
+ 'UCG': 'Ser',
+ 'UCU': 'Ser',
+ 'UGA': 'TER',
+ 'UGC': 'Cys',
+ 'UGG': 'Trp',
+ 'UGU': 'Cys',
+ 'UUA': 'Leu',
+ 'UUC': 'Phe',
+ 'UUG': 'Leu',
+ 'UUU': 'Phe'}
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.argument('codonw-table')
@@ -23,8 +90,8 @@ def codonw_to_table(codonw_table, output_table=None):
             else:
                 vec = l.strip().split()
                 for i, v in enumerate(vec):
-                    if re.search('[A|T|C|G]{3}', v):
-                        d.append({'codon': v,'val1':vec[i + 1],'val2':vec[i+2]})
+                    if re.search('[A|T|C|G|U]{3}', v):
+                        d.append({'codon': v,'AA':codons[v],'val1':vec[i + 1],'val2':vec[i+2]})
 
     dflist = []
     for i in big_dict.keys():
@@ -33,7 +100,8 @@ def codonw_to_table(codonw_table, output_table=None):
         dflist.append(df)
 
     out_tbl = pd.concat(dflist)
-    out_tbl[['orf','codon','val1','val2']].to_csv(output_table, index=False)
+    #out_tbl = out_tbl.merge(pd.DataFrame(codons), on='codon', how='outer')[['orf','codon','aa','val1','val2']]
+    out_tbl[['orf','AA','codon','val1','val2']].to_csv(output_table, index=False)
 
 
 if __name__=='__main__':
